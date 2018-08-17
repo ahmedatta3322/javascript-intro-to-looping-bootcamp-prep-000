e366d04169ff3ab309b86e9867531010710ceda4
ahmed atta <ahmedatta3322@gmail.com>
2018-08-17 13:14:30 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(n){
+  n.push("I am 1 strange loop.")
+  for(let i = 1 ; i < 25 ;i++){
+    n.push("I am " +i+" strange loops.")
+  }
+  return n
+}
+function whileLoop(n){
+  while(n > 0){
+    console.log(n)
+    n = n -1
+  }
+  return "done"
+}
+function doWhileLoop(array) {
+	var i = 0;
+
+	function incrementVariable() {
+		i = i + 1;
+	}
+
+	do {
+		console.log('array.length = ' + array.length + ' and i = ' + i);
+		array = array.slice(1);
+		incrementVariable();
+	} while (array.length > 0 && i < 5);
+
+	return array;
+}
